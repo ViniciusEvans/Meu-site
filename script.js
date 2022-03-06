d7c95e513e7a1cc8e3f5290dcf2bf39b91eb17c6
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-06 16:48:33 -0300

    features novas

--- script.js
+++ script.js
@@ -1,39 +1,68 @@
-let lastKnownScrollPosition = 0;
+let scrollPosition = 0;
 let ticking = false;
-const btnAboutme = document.querySelector('.aboutme');
-const btnSkill = document.querySelector('.skills');
-const btnEducation = document.querySelector('.education');
-const btnPortfolio = document.querySelector('.portfolio');
-const btnContacts = document.querySelector('.contacts');
+const header = document.querySelector("header");
+const btnAboutme = document.querySelector(".aboutme");
+const btnSkill = document.querySelector(".skills");
+const btnEducation = document.querySelector(".education");
+const btnPortfolio = document.querySelector(".portfolio");
+const btnContacts = document.querySelector(".contacts");
 
-function doSomething(scrollPos) {
-    const header = document.querySelector('header')
-    if (lastKnownScrollPosition >= 100) {
-        header.style.backgroundColor = '#FAFAFA'
-        header.style.boxShadow = "2.8px 2.8px 2.2px rgba(0, 0, 0, 0.02), 6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028), 12.5px 12.5px 10px rgba(0, 0, 0, 0.035), 22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042);"
-    } else {
-        header.style.backgroundColor = 'transparent'
-    }
+const about = document.querySelector(".section-about");
+const skill = document.querySelector(".section-skills");
+const formation = document.querySelector(".section-formation");
+const projects = document.querySelector(".section-projects");
+const contact = document.querySelector(".section-contact");
+
+function changeBackgroundColorHeader(scrollPos) {
+  if (scrollPos >= 100) {
+    header.style.backgroundColor = "#fff";
+    header.style.boxShadow =
+      "2.8px 2.8px 2.2px rgba(0, 0, 0, 0.02), 6.7px 6.7px 5.3px rgba(0, 0, 0, 0.028), 12.5px 12.5px 10px rgba(0, 0, 0, 0.035), 22.3px 22.3px 17.9px rgba(0, 0, 0, 0.042);";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 }
-document.addEventListener('scroll', function(e) {
-    lastKnownScrollPosition = window.scrollY;
-    console.log(lastKnownScrollPosition)
-    if (!ticking) {
-        window.requestAnimationFrame(function() {
-            doSomething(lastKnownScrollPosition);
-            ticking = false;
-        });
-        ticking = true;
-    }
+document.addEventListener("scroll", function (e) {
+  scrollPosition = window.scrollY;
+  changeBackgroundColorHeader(scrollPosition);
+  buttonFocusBySection([
+    [about, btnAboutme],
+    [skill, btnSkill],
+    [formation, btnEducation],
+    [projects, btnPortfolio],
+    [contact, btnContacts],
+  ]);
 });
 
-const scrollingTo = pos => {
-    console.log(pos)
-    window.scrollTo(0, pos)
+function scrollingTo(scrollPos) {
+  console.log(scrollPos);
+  window.scrollBy(0, scrollPos);
 }
-console.log(btnAboutme)
-btnAboutme.addEventListener('click', () => scrollingTo(719))
-btnSkill.addEventListener('click', () => scrollingTo(1014))
-btnEducation.addEventListener('click', () => scrollingTo(1626))
-btnPortfolio.addEventListener('click', () => scrollingTo(2196))
-btnContacts.addEventListener('click', () => scrollingTo(2840))
+btnAboutme.addEventListener("click", () =>
+  scrollingTo(about.getBoundingClientRect().y - 163)
+);
+btnSkill.addEventListener("click", () =>
+  scrollingTo(skill.getBoundingClientRect().y - 163)
+);
+btnEducation.addEventListener("click", () =>
+  scrollingTo(formation.getBoundingClientRect().y - 163)
+);
+btnPortfolio.addEventListener("click", () =>
+  scrollingTo(projects.getBoundingClientRect().y - 163)
+);
+btnContacts.addEventListener("click", () =>
+  scrollingTo(contact.getBoundingClientRect().y - 163)
+);
+
+function buttonFocusBySection(array) {
+  array.forEach((element) => {
+    if (
+      element[0].getBoundingClientRect().top <= 163 &&
+      element[0].getBoundingClientRect().bottom >= 163
+    ) {
+      element[1].focus();
+    } else {
+      element[1].blur();
+    }
+  });
+}
